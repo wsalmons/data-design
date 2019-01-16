16b28d56235f85ac4321930fe562977a26f1edb3
Wyatt Salmons <wyattsalmons@gmail.com>
2019-01-16 15:33:41 -0700

    changed location of use case section in auden-ignatious.php to use-case..php. added to index page to reflect references to auden-ignation.php and use-case.php.

--- epic/use-case.php
+++ epic/use-case.php
@@ -7,7 +7,7 @@
 	<body>
 		<h1>Use Case</h1>
 		<p><b>Title:</b> Posting on Flickr</p>
-		<p><b>Description:</b> Auden posts an uploaded picture to Flickr.com</p>
+		<p><b>Description:</b> Auden posts an uploaded picture from his computer to his Flickr.com account</p>
 		<p><b>Name of User and their Role</b> Auden Ignatious, posting image to Flickr.com</p>
 		<p><b>Usage Preconditions:</b> Auden needs a computer with stored file to upload, an internet connection, a Flickr account.</p>
 		<p><b>Usage Postconditions:</b> Auden successfully posts his image to his Flickr account.</p>
@@ -28,5 +28,25 @@
 			<li>A new page is loaded with the uploaded image displayed on it.</li>
 		</ul>
 		<h3><b>Frequency of Use:</b> 3-5 times a week.</h3>
+		<hr>
+		<h1>Conceptual Model</h1>
+		<h3>User</h3>
+		<ul>
+			<li>user</li>
+			<li>userId</li>
+			<li>userEmail</li>
+			<li>userName</li>
+			<li>userPassword</li>
+		</ul>
+		<h3>Photo</h3>
+		<ul>
+			<li>photo</li>
+			<li>photoId</li>
+			<li>photoIdUserID</li>
+			<li>photoUrl</li>
+			<li>photoSize</li>
+			<li>photoCameraMetadata</li>
+			</ul>
+
 	</body>
 </html>
