fce29cac19f8838a925debe400a1d010556836d6
Wyatt Salmons <wyattsalmons@gmail.com>
2019-01-16 14:04:07 -0700

    added text

--- epic/auden-ignatious.php
+++ epic/auden-ignatious.php
@@ -14,7 +14,7 @@
 			<li><b>Occupation:</b> Photographer, YouTube Creator</li>
 			<li><b>Location:</b> Albuquerque, NM</li>
 			<li><b>Hobbies:</b> Attending open mics for slam poetry, slouchy beanies, deeeep-V tee shirts</li>
-			<li><b>Likes:</b> Street art, portrait photography, ironic indie bands</li>
+			<li><b>Likes:</b> Street photography, portrait photography, ironic indie bands</li>
 			<li><b>Owned Technology:</b></li>
 				<ul>
 					<li><b>Phone:</b> iPhone 10, cracked screen (reads slam poetry from iPhone while performing at open mic)</li>
@@ -22,6 +22,27 @@
 					<li><b>Camera:</b> Nikon D750, C firmware: Ver. 1.01</li>
 				</ul>
 			<li><b>Technology Comfort Level:</b> Very comfortable with consumer technology, social networking, digital media tools</li>
+			<li><b>Goals:</b> Interested in online social networking platform with a focus on high quality image hosting and sharing, with robust
+				<br>cataloging and search features.</li>
+			<li><b>Technological Needs:</b> Easy upload, edit, sharing, and social networking features.</li>
+			<li><b>Frustrations</b></li>
+				<ul>
+					<li>Poor upload quality</li>
+					<li>Inability to edit captions, or lacking ability to update posting with updated image files</li>
+					<li>Lacking robust search, save or explore features</li>
+				</ul>
 		</ul>
+		<hr>
+		<h3>User Story</h3>
+		<p>Auden has been taking some very impressive photos lately and would like to post his work on Flickr.com</p>
+		<hr>
+		<h3>Use Case</h3>
+		<p><b>Title:</b> Posting on Flickr</p>
+		<p><b>Description:</b> Auden posts an uploaded picture to Flickr.com</p>
+		<p><b>Name of User and their Role</b> Auden Ignatious, posting image to Flickr.com</p>
+		<p><b>Usage Preconditions:</b> Auden needs a computer with stored file to upload, an internet connection, a Flickr account.</p>
+		<p><b>Usage Postconditions:</b> Auden successfully posts his image to his Flickr account.</p>
+		<hr>
+		<h3>Interaction Flow</h3>
 	</body>
 </html>
