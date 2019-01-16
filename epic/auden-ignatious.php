11a3820d91273808cd8f0628935a2ce0b66c0557
Wyatt Salmons <wyattsalmons@gmail.com>
2019-01-16 13:31:53 -0700

    added text to index

--- epic/auden-ignatious.php
+++ epic/auden-ignatious.php
@@ -5,6 +5,23 @@
 		<title>Customer Persona</title>
 	</head>
 	<body>
-		<img src="auden-ignatious.jpg" alt="auden-ignatious.jpg">
+		<h1>Customer Persona</h1>
+		<img src="auden-ignatious.jpg" alt="auden-ignatious.jpg"
+			  style="width:300px; height:300px">
+		<ul>
+			<li><b>Name:</b> Auden Ignatious</li>
+			<li><b>Age:</b> 23</li>
+			<li><b>Occupation:</b> Photographer, YouTube Creator</li>
+			<li><b>Location:</b> Albuquerque, NM</li>
+			<li><b>Hobbies:</b> Attending open mics for slam poetry, slouchy beanies, deeeep-V tee shirts</li>
+			<li><b>Likes:</b> Street art, portrait photography, ironic indie bands</li>
+			<li><b>Owned Technology:</b></li>
+				<ul>
+					<li><b>Phone:</b> iPhone 10, cracked screen (reads slam poetry from iPhone while performing at open mic)</li>
+					<li><b>Laptop: </b> 2018 MacBook Pro 15, macOS 10.14: Mojave (Liberty)</li>
+					<li><b>Camera:</b> Nikon D750, C firmware: Ver. 1.01</li>
+				</ul>
+			<li><b>Technology Comfort Level:</b> Very comfortable with consumer technology, social networking, digital media tools</li>
+		</ul>
 	</body>
 </html>
