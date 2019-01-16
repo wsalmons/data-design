0e0d15a482f2f3b67d5d61424a6a2103f937b6fe
Wyatt Salmons <wyattsalmons@gmail.com>
2019-01-16 14:21:12 -0700

    added text

--- epic/auden-ignatious.php
+++ epic/auden-ignatious.php
@@ -44,5 +44,17 @@
 		<p><b>Usage Postconditions:</b> Auden successfully posts his image to his Flickr account.</p>
 		<hr>
 		<h3>Interaction Flow</h3>
+		<ol>
+			<li>Auden enters Flickr.com into his web browser</li>
+			<li>The server receives his computers request, and displays Audens home page.</li>
+			<li>Auden is already logged into his Flikr.com account, indicated by his profile picture displayed in the top right of the page.</li>
+			<li>Auden clicks the cloud/arrow icon in the top right to begin the process of uploading his file, which results a new page.</li>
+			<li>Auden clicks on the blue "Choose photos and videos to upload" button on the resulting page, which results a system window. </li>
+			<li>Auden navigates with his system window, and selects the file he would like to upload.</li>
+			<li>A new page is loaded, displaying a thumbnail of his new file.</li>
+			<li>Auden selects an album to upload the file to by selcting "add to albums" and searching in the resulting search bar for the intended album and selecting it.</li>
+			<li>Auden adds his files to various groups buy selecting the button "Add to groups" and searching for and selecting the intended groups</li>
+			<li></li>
+		</ol>
 	</body>
 </html>
