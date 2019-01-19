26b501cf854867911262b501f7f0a92e383923c2
Wyatt Salmons <wyattsalmons@gmail.com>
2019-01-18 18:24:36 -0700

    insert to database

---
+++ mysql/oml.sql
@@ -0,0 +1,8 @@
+INSERT INTO user (userId, userIdName, userIdEmail, userIdPassword) VALUES (UNHEX("3be684fca1ae44b1a0720273c69c1c01"),
+"wyguyy", "email@email.com", "ahahahahahahahahahahahahahahahah");
+
+INSERT INTO photo (photoId, photoIdUserId, photoIdUrl, photoIdFileType, photoIdMetadata) VALUES (UNHEX("de78e2818aaa45c48905a3e52db1d95e"),
+UNHEX("3be684fca1ae44b1a0720273c69c1c01"), "website.com/photo/photoId", "jpeg", "Wyatt Salmons");
+
+INSERT INTO `group` (groupId, groupIdPhotoId, groupIdUserId, groupIdName) VALUES (UNHEX("2b4c17b94ecc46309383491997d849d1"), UNHEX("de78e2818aaa45c48905a3e52db1d95e"),
+UNHEX("3be684fca1ae44b1a0720273c69c1c01"), "Cool Photo Group!");
